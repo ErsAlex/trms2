3f6d75891dd6cb0de60ecb0deb6383f5b2aefb60
Alex Ershov <galak654@gmail.com>
2023-11-05 22:50:25 +0300

    added Crud user_service

--- services/users/user_service.py
+++ services/users/user_service.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, update, delete
 from common.database.base_service import BaseDataBaseService
 from common.utils.hashing import get_password_hash
 from models.models import User
@@ -23,6 +24,37 @@ class UserDatabaseService(BaseDataBaseService):
             await session.add(new_user)
             await session.commit()
             return new_user
-     
+        
+        async def update_user(
+            self,
+            session: AsyncSession,
+            filter_by,
+            data: dict,
+        ):
+            stmt = update(User).values(**data).filter_by(**filter_by).returning(User)
+            updated_user = await session.execute(stmt)
+            updated_user = updated_user.scalar_one_or_none()
+            return updated_user
+        
+        async def get_user(
+            self,
+            session: AsyncSession,
+            filter_by
+        ):            
+            stmt = select(User).filter_by(**filter_by)
+            user = await session.execute(stmt)
+            user = user.scalar_one_or_none()
+            return user
+        
+        async def delete_user(
+            self,
+            session: AsyncSession,
+            filter_by
+        ):
+            stmt = delete(User).filter_by(**filter_by)
+            await session.execute(stmt)
+            return {"user_deleted": True}
+        
+        
 def get_service():
     return UserDatabaseService(dsn=UserDatabaseSettings.db_dsn)
